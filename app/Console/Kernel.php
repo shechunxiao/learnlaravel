a3d9d27e3f62dc838048513be89c3a7211cb7191
scx <282074579@qq.com>
2020-04-16 14:19:59 +0800

    测试

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('SecondConsole')->everyMinute();
+        $schedule->command('SecondConsole')->everyMinute()->withoutOverlapping()->sendOutputTo('./secondconsole.txt');
     }
 
     /**
