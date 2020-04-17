5265f232569afb519adeb9ddff1d2b6aa818b308
scx <282074579@qq.com>
2020-04-17 08:18:10 +0800

    测试

---
+++ app/Http/Controllers/FirstController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jobs\ProcessPodcast;
+use Illuminate\Http\Request;
+
+class FirstController extends Controller
+{
+    //
+    public function index(){
+        file_put_contents('./test.txt',"测试/r/n",FILE_APPEND);
+    }
+    public function job(){
+        ProcessPodcast::dispatch(1);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::any('/test','FirstController@index');
+Route::any('/job','FirstController@job');
